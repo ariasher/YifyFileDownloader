5a76c69f5d97788f9f5525a21d062ada6fc377f9
Mukul Khanna <mukulkhanna.96@gmail.com>
2023-11-01 20:36:08 +0530

    info label update fix

--- Forms/YTS Downloader.cs
+++ Forms/YTS Downloader.cs
@@ -35,85 +35,89 @@ namespace YifyFileDownloader.Forms
             _apiService = new ApiService(apiSettings, serviceLogger, context);
         }
 
-        private async Task btnDownload_Click(object sender, EventArgs e)
+        private void btnDownload_Click(object sender, EventArgs e)
         {
-            var now = DateTime.Now;
-
-            InstanceLog instanceLog = new InstanceLog
+            Task.Run(async () =>
             {
-                CreatedAt = now,
-                UpdatedAt = now,
-                IsSuccess = false
-            };
+                var now = DateTime.Now;
 
-            try
-            {
-                infoLabel.Text = "Download is going to start.";
-                _logger.LogInformation("Download is going to start.");
-
-                int page = 1;
-                bool reachedLastRead = false;
-
-                // Fetch when was the last time this program ran successfully.
-                var lastRunDateTime = _context.InstanceLogs
-                    .OrderByDescending(i => i.Id)
-                    .Where(i => i.IsSuccess)
-                    .FirstOrDefault()?.CreatedAt;
-
-                int lastMovieId = _context.MovieDetails
-                    .OrderBy(m => m.Id)
-                    .Where(m => m.CreatedAt == lastRunDateTime)
-                    .AsNoTracking()
-                    .FirstOrDefault()?.MId ?? 0;
-
-                while (!reachedLastRead)
+                InstanceLog instanceLog = new InstanceLog
                 {
-                    _logger.LogInformation("API loop started.");
-                    infoLabel.Text = "Started calling API.";
+                    CreatedAt = now,
+                    UpdatedAt = now,
+                    IsSuccess = false
+                };
 
-                    var apiResponse = await _apiService.GetApiMoviesResponse(page);
-                    _logger.LogInformation($"API response received for page {page}.");
+                try
+                {
 
-                    (bool hasMovieId, List<MovieDetails> movieDetails) = MapApiData(apiResponse, lastMovieId, now);
-                    _logger.LogInformation($"API response mapped for page {page}.");
-                    _logger.LogInformation($"Saving details to the DB.");
+                    infoLabel.PerformSafely(() => infoLabel.Text = "Download is going to start."); 
+                    _logger.LogInformation("Download is going to start.");
 
-                    await _context.AddRangeAsync(movieDetails);
-                    _context.SaveChanges();
-                    _logger.LogInformation($"Saved details to the DB.");
+                    int page = 1;
+                    bool reachedLastRead = false;
 
-                    // Wait for 5 seconds to refetch data of the next page
-                    ++page;
-                    _logger.LogInformation($"Going to sleep for 5 seconds.");
-                    Thread.Sleep(5000);
+                    // Fetch when was the last time this program ran successfully.
+                    var lastRunDateTime = _context.InstanceLogs
+                        .OrderByDescending(i => i.Id)
+                        .Where(i => i.IsSuccess)
+                        .FirstOrDefault()?.CreatedAt;
+
+                    int lastMovieId = _context.MovieDetails
+                        .OrderBy(m => m.Id)
+                        .Where(m => m.CreatedAt == lastRunDateTime)
+                        .AsNoTracking()
+                        .FirstOrDefault()?.MId ?? 0;
+
+                    while (!reachedLastRead)
+                    {
+                        _logger.LogInformation("API loop started.");
+                        infoLabel.PerformSafely(() => infoLabel.Text = "Started calling API.");
+
+                        var apiResponse = await _apiService.GetApiMoviesResponse(page);
+                        _logger.LogInformation($"API response received for page {page}.");
+
+                        (bool hasMovieId, List<MovieDetails> movieDetails) = MapApiData(apiResponse, lastMovieId, now);
+                        _logger.LogInformation($"API response mapped for page {page}.");
+                        _logger.LogInformation($"Saving details to the DB.");
+
+                        await _context.AddRangeAsync(movieDetails);
+                        _context.SaveChanges();
+                        _logger.LogInformation($"Saved details to the DB.");
+
+                        // Wait for 5 seconds to refetch data of the next page
+                        ++page;
+                        _logger.LogInformation($"Going to sleep for 5 seconds.");
+                        Thread.Sleep(5000);
+                    }
+
+                    _logger.LogInformation("Download finished. Data is upto-date.");
+
+                    infoLabel.PerformSafely(() => infoLabel.Text = "Download finished. Data is upto-date.");
+                    Dialog.ShowMessage(Utility.TitleSuccess, "Download finished.", Dialog.Type.Information);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"Error while trying to download data.");
+                    _logger.LogError($"Exception : {ex} with message {ex.Message}.");
+                    infoLabel.PerformSafely(() => infoLabel.Text = ex.Message);
+                    instanceLog.UpdatedAt = DateTime.Now;
+                    Dialog.ShowMessage(Utility.TitleError, "An error occurred while downloading data. Please try again or check log files.", Dialog.Type.Error);
                 }
 
-                _logger.LogInformation("Download finished. Data is upto-date.");
-                infoLabel.Text = "Download finished. Data is upto-date.";
-                Dialog.ShowMessage(Utility.TitleSuccess, "Download finished.", Dialog.Type.Information);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Error while trying to download data.");
-                _logger.LogError($"Exception : {ex} with message {ex.Message}.");
-                infoLabel.Text = ex.Message;
-                instanceLog.UpdatedAt = DateTime.Now;
-                Dialog.ShowMessage(Utility.TitleError, "An error occurred while downloading data. Please try again or check log files.", Dialog.Type.Error);
-            }
-
-            try
-            {
-                _logger.LogInformation("Saving instance log.");
-                await SaveInstanceLog(instanceLog);
-                _logger.LogInformation("Intance log saved.");
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Error saving instance log.");
-                _logger.LogError($"Exception : {ex} with message {ex.Message}.");
-            }
+                try
+                {
+                    _logger.LogInformation("Saving instance log.");
+                    await SaveInstanceLog(instanceLog);
+                    _logger.LogInformation("Intance log saved.");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"Error saving instance log.");
+                    _logger.LogError($"Exception : {ex} with message {ex.Message}.");
+                }
+            });
         }
-
 
         private async Task SaveInstanceLog(InstanceLog log)
         {
@@ -181,6 +185,21 @@ namespace YifyFileDownloader.Forms
             }
 
             return (hasLastMovieId, movies); 
+        }
+
+        /// <summary>
+        /// Helper method to determin if invoke required, if so will rerun method on correct thread.
+        /// if not do nothing.
+        /// </summary>
+        /// <param name="c">Control that might require invoking</param>
+        /// <param name="a">action to preform on control thread if so.</param>
+        /// <returns>true if invoke required</returns>
+        public bool ControlInvokeRequired(Control c, Action a)
+        {
+            if (c.InvokeRequired) c.Invoke(new MethodInvoker(delegate() { a(); }));
+            else return false;
+
+            return true;
         }
     }
 }
---
+++ Utilities/CrossThreadExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YifyFileDownloader.Utilities
+{
+    public static class CrossThreadExtensions
+    {
+        public static void PerformSafely(this Control target, Action action)
+        {
+            if (target.InvokeRequired)
+            {
+                target.BeginInvoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        public static void PerformSafely<T1>(this Control target, Action<T1> action, T1 parameter)
+        {
+            if (target.InvokeRequired)
+            {
+                target.BeginInvoke(action, parameter);
+            }
+            else
+            {
+                action(parameter);
+            }
+        }
+
+        public static void PerformSafely<T1, T2>(this Control target, Action<T1, T2> action, T1 p1, T2 p2)
+        {
+            if (target.InvokeRequired)
+            {
+                target.BeginInvoke(action, p1, p2);
+            }
+            else
+            {
+                action(p1, p2);
+            }
+        }
+
+    }
+}
