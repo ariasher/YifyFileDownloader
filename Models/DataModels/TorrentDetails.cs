58b9bfea804a610b212c9aafab0c4357a808a9a1
Mukul Khanna <mukulkhanna.96@gmail.com>
2023-11-01 14:37:32 +0530

    added torrent model

---
+++ Models/DataModels/TorrentDetails.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YifyFileDownloader.Models.DataModels;
+
+[Table("TORRENT_DETAILS")]
+public class TorrentDetails : BaseModel
+{
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; }
+
+    [Required]
+    [StringLength(200)]
+    public string URL { get; set; }
+
+    [Required]
+    public int Year { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Genre { get; set; }
+
+    [Required]
+    public double Rating { get; set; }
+}
