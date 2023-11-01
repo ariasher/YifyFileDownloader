e4653fe6e901e3a7d6c83c79b4c0e64b895b4eba
Mukul Khanna <mukulkhanna.96@gmail.com>
2023-11-01 14:33:26 +0530

    added base model and api models

---
+++ Models/DataModels/API.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YifyFileDownloader.Models.DataModels;
+
+[Table("API")]
+public class API : BaseModel
+{
+    [Required]
+    [StringLength(100)]
+    public string ApiName { get; set; }
+
+    [Required]
+    [StringLength(200)]
+    public string ApiEndpoint { get; set; }
+
+    [Required]
+    [StringLength(400)]
+    public string Payload { get; set; }
+
+    [Required]
+    [StringLength(2000)]
+    public string Response { get; set; }
+}
---
+++ Models/DataModels/BaseModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YifyFileDownloader.Models.DataModels;
+
+public abstract class BaseModel
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; }
+
+    [Required]
+    [DefaultValue(true)]
+    public bool IsActive { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+
+    [Required]
+    public DateTime CreatedAt { get; set; }
+
+    [Required]
+    public DateTime UpdatedAt { get; set; }
+}
