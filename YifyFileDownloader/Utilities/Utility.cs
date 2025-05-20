b1516e33806307d43cb3f9878ca451d0f9d30193
Mukul Khanna <mukulkhanna.96@gmail.com>
2025-05-20 20:32:02 +0530

    Project structure changes, addition of web api and tests

--- YifyFileDownloader/Utilities/Utility.cs
+++ YifyFileDownloader/Utilities/Utility.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace YifyFileDownloader.Utilities
+﻿namespace YifyFileDownloader.Utilities
 {
     public static class Utility
     {
-
         #region CONSTANT NAMES
 
         public static string ConnectionStringName = "YTSConnectionString";
---
+++ YifyTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace YifyTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
